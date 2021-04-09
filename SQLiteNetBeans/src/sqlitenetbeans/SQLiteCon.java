2eccdd3ebfaad70c2a704564c10f5a3d66c6e319
Pedro B <bereilhp@gmail.com>
2021-04-09 22:34:59 +0200

    SQLite and Java Connectivity

---
+++ SQLiteNetBeans/src/sqlitenetbeans/SQLiteCon.java
@@ -0,0 +1,39 @@
+package sqlitenetbeans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class SQLiteCon {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String mysqlURL = "jdbc:sqlite:chinook.db";
+            Connection conexion = DriverManager.getConnection(mysqlURL);
+            
+            Statement myStatement = conexion.createStatement();
+            ResultSet result = myStatement.executeQuery("SELECT * FROM Artists");
+            
+            int id;
+            String nombre;
+            while(result.next()){
+                id = result.getInt("ArtistID");
+                nombre = result.getString("Name");
+                System.out.println("Artista " + id + " -- Nombre: " + nombre);
+            }
+
+            conexion.close();
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error cargando driver: " + ex);
+        } catch (SQLException ex) {
+            System.out.println("Error de SQL: " + ex);
+        }
+        
+    }
+    
+}
